bcd57ff011d1838372b9cd91221e6ca319d5be07
Adarsh Gour <adarshgour082@gamil.com>
2026-02-20 21:04:16 +0530

    updated Bookmyshow project

--- BookMyShow/project/myapp/models.py
+++ BookMyShow/project/myapp/models.py
@@ -1,3 +1,23 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
-
 # Create your models here.
+
+class User(AbstractUser):
+    ROLE_CHOICES = (
+        ('superadmin', 'Super Admin'),
+        ('admin', 'Admin'),
+        ('user', 'User'),
+    )
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='user')
+    def __str__(self):
+        return self.username
+    
+
+class Movie(models.Model):
+        title = models.CharField(max_length=100)
+        description = models.TextField()
+        release_date = models.DateField()
+        image = models.ImageField(upload_to='movies/',null=True, blank=True)
+
+        def __str__(self):
+            return self.title
--- BookMyShow/project/myapp/views.py
+++ BookMyShow/project/myapp/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from .models import Movie, User
 # Create your views here.
+
+def home(request):
+    movies = Movie.objects.all()
+    return render(request, 'home.html')
